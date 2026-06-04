53e690c8824caaac5a39d0f2ff6d83b341d703ba
PRAIZZZ-08 <praizzzdev@gmail.com>
2026-06-04 17:51:54 +0100

    feat: add checker logic for conncurrent logging

--- monitor/main.go
+++ monitor/main.go
@@ -4,8 +4,9 @@ import  (
 	"fmt"
 	"monitor/network"
 	"time"
+	"sync"
 	
-	//"github.com/fatih/color"
+	"github.com/fatih/color"
 )
 
 type Reportable interface {
@@ -22,18 +23,49 @@ func (e Entry) Report() string {
 }
 
 
-func LogComponent(arg Reportable) {
-rawReport := arg.Report()
-coloredReport := color.GreenString(rawReport)
-time.Sleep(1 * time.Second)
-fmt.Println(coloredReport)
+func LogComponent(arg Reportable, res chan string) {
+    var id int
+    switch v := arg.(type) {
+    case Entry:
+        id = v.ID
+    case network.Node:
+        id = v.ID
+    }
+    if id == 234 {
+        time.Sleep(3 * time.Second)
+    }
+    
+    rawReport := arg.Report()
+    coloredReport := color.GreenString(rawReport)
+    
+    fmt.Println(coloredReport)   
+    res <- rawReport
 }
 
 func main() {
-e1 := Entry{ID: 46, Value: 65}
+var wg sync.WaitGroup
+resChan :=  make(chan string)
+
+e1 := []Entry{
+	{ID: 46, Value: 65.1},
+	{ID: 34, Value: 93.4},
+	{ID: 96, Value: 86.5},
+	{ID: 234, Value: 76.0},
+}
+
 n1 := network.Node{ID: 234, IP: "192.168.0.1"}
 
-go LogComponent(e1)
-go LogComponent(n1)
-time.Sleep(2 * time.Second)
+for _, p := range e1 {
+        go LogComponent(p, resChan)
+        go LogComponent(n1, resChan)
+   }
+
+  for i := 0; i < len(e1); i++ {
+   select {
+    case msg := <-resChan:
+     fmt.Println("Received:", msg)
+     case <-time.After(1 * time.Second):
+     fmt.Println("ERROR: Component response timed out.")
+   }
+  }
 }
